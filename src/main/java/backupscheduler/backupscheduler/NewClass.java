6de19f8207e3979dc8b0602971f200cf827e0b0e
MarcoGonzalezE <marco.gonzalez.grupoalvamex@gmail.com>
2017-12-07 17:31:00 -0600

    Conexiones2

---
+++ src/main/java/backupscheduler/backupscheduler/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backupscheduler.backupscheduler;
+
+/**
+ *
+ * @author lenovo
+ */
+public class NewClass {
+    
+}
